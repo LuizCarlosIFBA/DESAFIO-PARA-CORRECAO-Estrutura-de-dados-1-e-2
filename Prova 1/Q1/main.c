ba6b0721a021e0ce36ef60931fca2218d63706f0
Luiz Carlos <sacramento.tecnico@hotmail.com>
2018-10-13 07:31:41 -0300

    Add files via upload

--- Prova 1/Q1/main.c
+++ Prova 1/Q1/main.c
@@ -1,45 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct nodo
-{
-   char info;
-   struct nodo *prox;
+typedef struct nodo{
+	char info;
+	struct nodo *prox;
+}Nodo; 
 
-} Nodo;
-
-void inserir(Nodo **inicio,char letra)
-{
-    Nodo *atual, *novo, *anterior;
-    novo = (Nodo *) malloc(sizeof(Nodo));
-    atual = *inicio;
-    anterior = NULL;
-
-      
-    novo->info = letra;
-    
-  
-    //Já que temos apenas 1 valor A e 2 valores B
-    //as únicas sequências possíveis são: ABB ou BBA.
-    while(atual != NULL && atual->info < letra){
-    	anterior = atual;
-        atual = atual->prox;
-    }
-    //A lógica acima, por coincidência atendeu o caso, apesar dela servir
-    //para unicamente ordenar em ordem alfabética. Obs: se usar esse código	
-    //para fins didáticos procure uma solução melhor para essa ordenação  
-
-    novo->prox = atual;
-  	
-    if(anterior == NULL){
-    	*inicio = novo;
-    } else{
-         anterior->prox = novo;
-    }
+//Insere no início
+void insereH(Nodo **topo,char ele){
+	Nodo *novo;
+	novo = malloc(sizeof(Nodo));
+	
+	novo->info = ele;
+	if(*topo==NULL){
+		//insere no topo e faz o próximo virar nulo
+		novo->prox = NULL;
+		*topo = novo; 
+	}else{	//fez o topo apontar pra celula 
+		novo->prox = *topo;
+		*topo = novo; 
+	}
 }
 
-
-void mostraLista(Nodo *inicio){
+void mostrar(Nodo *inicio){
     while(inicio != NULL){
         printf("%c ", inicio->info);
         inicio = inicio->prox;
@@ -47,12 +30,19 @@ void mostraLista(Nodo *inicio){
 }
 
 
+//Excluir elementos
+void excluir_elemento(Nodo *topo){
+	//implemente
+}
+
+
 void main(){
-    Nodo *inicio = NULL;
-   
-    inserir(&inicio,'B');
-    inserir(&inicio,'A');
-    inserir(&inicio,'B');
-    
-    mostraLista(inicio);
+	//aponta para o topo	
+	Nodo *topo = NULL;
+	insereH(&topo,'A');
+	insereH(&topo,'B');	
+	insereH(&topo,'B');
+	insereH(&topo,'B');
+	mostrar(topo);
+	
 }
